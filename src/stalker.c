c839ca7b9267c93848e5367c8e43121bc60fc87a
tetsuo <ogunduz@gmail.com>
2014-12-22 01:59:13 +0200

    comments

--- src/stalker.c
+++ src/stalker.c
@@ -7,9 +7,9 @@
 #include "uv.h"
 
 #define VERSION "0.1.2"
-#define INTERVAL 100
 #define ARGS_MAX 128
 
+// cleanup before exiting
 #define MAKE_VALGRIND_HAPPY() \
   do { \
     close_loop(uv_default_loop()); \
@@ -26,17 +26,19 @@ static void handle_close_walk(uv_handle_t *handle, void *arg);
 static void handle_exit(uv_process_t *req, int64_t exit_status, int term_signal);
 static void close_loop(uv_loop_t *loop);
 static void init_fs_event(const char *file);
-static void redirect_stdout(const char *path);
 static int option(const char *small, const char *large, const char *arg);
 static void usage();
 
 static uv_loop_t *loop;
 static uv_process_t process;
 static uv_process_options_t options;
+
+static int process_running = 0;
 static char *cmd[4] = { "sh", "-c", NULL, NULL };
-static int quiet = 0;
-static int halt = 0;
-static int verbose = 0;
+
+static int quiet = 0; // only output stderr
+static int halt = 0; // halt if cmd exits with 1
+static int verbose = 0; // makes the operation more talkative
 
 static void handle_close_walk(uv_handle_t *handle, void *arg) {
   if (!uv_is_closing(handle))
@@ -44,6 +46,7 @@ static void handle_close_walk(uv_handle_t *handle, void *arg) {
 }
 
 static void close_loop(uv_loop_t *loop) {
+  // walk the list of handles and request them to be closed
   uv_walk(loop, handle_close_walk, NULL);
 }
 
@@ -69,18 +72,11 @@ static void usage() {
   exit(1);
 }
 
-static void redirect_stdout(const char *path) {
-  int fd = open(path, O_WRONLY);
-  if (dup2(fd, 1) < 0) {
-    perror("dup2()");
-    EXIT1();
-  }
-}
-
 static void handle_exit(uv_process_t *process, int64_t exit_status,
   int term_signal) {
   fprintf(stderr, "Process exited with status %lld, signal %d\n", exit_status, term_signal);
   uv_close((uv_handle_t*) process, NULL);
+  process_running = 0;
 }
 
 static void handle_update (uv_fs_event_t *handle, const char *file,
@@ -89,59 +85,46 @@ static void handle_update (uv_fs_event_t *handle, const char *file,
   if (verbose) {
     fprintf(stderr, "\033[36m");
     if (events == UV_RENAME)
-        fprintf(stderr, "renamed");
+        fprintf(stderr, "rename:");
     if (events == UV_CHANGE)
-        fprintf(stderr, "changed");
-    fprintf(stderr, ": \033[0m\n \033[90mpath:\033[0m %s\n", handle->path);
+        fprintf(stderr, "change:");
+    fprintf(stderr, " \033[0m\033[90mpath\033[0m %s", handle->path);
     if (file)
-      fprintf(stderr, " \033[90mfile:\033[0m %s\n", file);
+      fprintf(stderr, " \033[90mfile\033[0m %s\n", file);
   }
 
-  int r;
-  if ((r = uv_spawn(loop, &process, &options))) {
-    fprintf(stderr, "%s\n", uv_strerror(r));
-    EXIT1();
-  }
-
-/*  pid_t pid;
-  int status;
-
-  if ((pid = fork()) < 0) {
-    fprintf(stderr, "fork()");
-    MAKE_VALGRIND_HAPPY();
-    exit(1);
-  } else if (pid == 0) {
-    if (quiet) redirect_stdout("/dev/null");
-    execvp(cmd[0], cmd);
+  if (process_running) {
+    fprintf(stderr, "already running\n");
   } else {
-    if (waitpid(pid, &status, 0) < 0) {
-      perror("waitpid()");
-      MAKE_VALGRIND_HAPPY();
-      exit(1);
+    int r;
+    if ((r = uv_spawn(loop, &process, &options))) {
+      fprintf(stderr, "%s\n", uv_strerror(r));
+      EXIT1();
+    } else {
+      process_running = 1;
     }
-    if (WEXITSTATUS(status)) {
-      fprintf(stderr, "\033[31mexit: %d\33[0m\n\n", WEXITSTATUS(status));
-      if (halt) {
-        MAKE_VALGRIND_HAPPY();
-        exit(WEXITSTATUS(status));
-      }
-    }
-  }*/
-
+  }
 }
 
 void init_fs_event(const char *file) {
+  // declare & initialize our fs event handle
   uv_fs_event_t *fs_event_req = malloc(sizeof(uv_fs_event_t));
   uv_fs_event_init(loop, fs_event_req);
+  // start watching for changes recursively
   uv_fs_event_start(fs_event_req, handle_update, file, UV_FS_EVENT_RECURSIVE);
 }
 
 int main(int argc, const char **argv){
-  signal(SIGPIPE, SIG_IGN);
+  // declare our main loop
   loop = uv_default_loop();
+
+  // ignore SIGPIPE
+  // see: http://pod.tst.eu/http://cvs.schmorp.de/libev/ev.pod#The_special_problem_of_SIGPIPE
+  signal(SIGPIPE, SIG_IGN);
 
   if (1 == argc) usage();
 
+  // collect args
   int len = 0;
   int interpret = 1;
   char *args[ARGS_MAX] = {0};
@@ -194,14 +177,19 @@ int main(int argc, const char **argv){
   }
 
   *(cmd + 2) = args[0];
+
+  // options to be passed to uv_spawn
   options.exit_cb = handle_exit;
   options.file = cmd[0];
   options.args = cmd;
 
   if (verbose)
-    fprintf(stderr, "\033[36m watching: \033[0m\033[90m%s\033[0m\n", args[1]);
+    fprintf(stderr, "\033[36mwatching \033[0m\033[90m%s\033[0m\n", args[1]);
   
+  // start monitoring the given path
   init_fs_event(args[1]);
+
+  // run our main loop
   uv_run(loop, UV_RUN_DEFAULT);
 
   return 0;
